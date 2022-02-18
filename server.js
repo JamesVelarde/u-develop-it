5bdb9340b26fbaedce8e42bbccc03c4237a05bf8
James <jimmyvelarde3302@gmail.com>
2022-02-17 21:58:22 -0700

    Updated candidate routes with a join query

--- server.js
+++ server.js
@@ -40,7 +40,11 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -90,8 +94,13 @@ app.post('/api/candidate', ({ body }, res) => {
     return;
   }
 
-  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-    VALUES (?,?,?)`;
+  const sql = `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
+  
   const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
